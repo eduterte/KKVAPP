94a04850e8a617d47d0b56292b0072c2f3963174
eduterte <eduterte@my.hpu.edu>
2014-02-28 17:14:27 -1000

    Splash and Home Screen
    App is able to run splash screen and transition into home screen that
    currently says "Hello world!"

---
+++ src/net/kkv/app/myMainScreen.java
@@ -0,0 +1,14 @@
+package net.kkv.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class myMainScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		//TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
