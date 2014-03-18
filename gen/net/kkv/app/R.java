75429f1a8e6bfcfab4257ec7f778cf7e09fe1f41
eduterte <eduterte@my.hpu.edu>
2014-03-17 17:13:00 -1000

    changed background/icon

--- gen/net/kkv/app/R.java
+++ gen/net/kkv/app/R.java
@@ -32,10 +32,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int lm_launcher=0x7f020001;
-        public static final int logical_model=0x7f020002;
-        public static final int ulu_hoku=0x7f020003;
+        public static final int calm=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_leaf=0x7f020002;
+        public static final int image_view=0x7f020003;
+        public static final int leaf=0x7f020004;
+        public static final int lm_launcher=0x7f020005;
+        public static final int logical_model=0x7f020006;
+        public static final int ulu_hoku=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f090004;
